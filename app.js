84248d84d6d95c78b536628f1bcb82b169527059
Martin Noble <martin.noble@ecx.io>
2017-02-08 11:34:08 +0000

    add watson streaming of pcm audio

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
 
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+//var fs = require('fs');
+//var ts = require('tailing-stream');
+//var gf = require('growing-file');
+
+var speech_to_text = new SpeechToTextV1 ({
+  username: 'aea0cb7f-cf30-480a-bf42-f0f79e415101',
+  password: '3vqJNwm6STIK'
+});
+
+var params = {
+  model: 'en-UK_BroadbandModel',
+  content_type: 'audio/l16; rate=48000',
+  continuous: true,
+  'interim_results': true,
+  'max_alternatives': 3,
+  'word_confidence': false,
+  timestamps: false,
+  keywords: ['colorado', 'tornado', 'tornadoes'],
+  'keywords_threshold': 0.5
+};
+
+
+
+
 var port = 3700;
 var outFile = 'demo.wav';
 var app = express();
@@ -25,19 +50,44 @@ binaryServer = BinaryServer({port: 9001});
 binaryServer.on('connection', function(client) {
   console.log('new connection');
 
-  var fileWriter = new wav.FileWriter(outFile, {
-    channels: 1,
-    sampleRate: 48000,
-    bitDepth: 16
-  });
+  //var fileWriter = new wav.FileWriter(outFile, {
+  //  channels: 1,
+  //  sampleRate: 48000,
+  //  bitDepth: 16
+  //});
 
   client.on('stream', function(stream, meta) {
     console.log('new stream');
-    stream.pipe(fileWriter);
+ 
+    // Create the stream.
+    var recognizeStream = speech_to_text.createRecognizeStream(params);
+
+    stream.pipe(recognizeStream);
+    recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
+    recognizeStream.setEncoding('utf8');
+
+    recognizeStream.on('results', function(event) { onEvent('Results:', event); });
+    recognizeStream.on('data', function(event) { onEvent('Data:', event); });
+    recognizeStream.on('error', function(event) { onEvent('Error:', event); });
+    recognizeStream.on('close', function(event) { onEvent('Close:', event); });
+    recognizeStream.on('speaker_labels', function(event) { onEvent('Speaker_Labels:', event); });
+
+    //stream.pipe(fileWriter);
 
     stream.on('end', function() {
-      fileWriter.end();
+      //fileWriter.end();
       console.log('wrote to file ' + outFile);
     });
+
+    
   });
 });
+
+
+
+// Displays events on the console.
+function onEvent(name, event) {
+  console.log(name, event);
+  console.log(name, JSON.stringify(event, null, 2));
+};
+
